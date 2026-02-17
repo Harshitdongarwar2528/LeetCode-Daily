78ec9f54b62611e808adfefb69e55f49495d94d4
Harshit Dongarwar <harshitdongarwar2003@gmail.com>
2026-02-17 15:49:42 +0530

    First Problem from leetCode consists of Brute force as well as optimal approach

---
+++ Easy/01-two-sum.java
@@ -0,0 +1,45 @@
+// Problem: 01. Two Sum
+// Link: https://leetcode.com/problems/two-sum/
+// Difficulty: Easy
+
+// Approach 1: Brute Force
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
+
+// Approach 2: Optimal (HashMap)
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+package Easy;
+import java.util.HashMap;
+
+class Solution {
+
+    // Optimal solution (used in submission)
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[]{};
+    }
+
+    // Brute force approach (for learning)
+    public int[] twoSumBrute(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+}
